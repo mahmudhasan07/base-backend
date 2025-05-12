2f6b2a171f44bd884d22761e4e3e099e17b0fb70
unknown <mahmudhasan.hb@gmail.com>
2025-05-12 19:54:26 +0600

    added social login system

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 import { compare } from "bcrypt";
 import { jwtHelpers } from "../../helper/jwtHelper";
 import { Secret } from "jsonwebtoken";
@@ -6,112 +6,212 @@ import ApiError from "../../error/ApiErrors";
 import { OTPFn } from "../../helper/OTPFn";
 import OTPVerify from "../../helper/OTPVerify";
 import { StatusCodes } from "http-status-codes";
+import { stripe } from "../../../config/stripe";
 
 const prisma = new PrismaClient();
-const logInFromDB = async (payload: { email: string, password: string, fcmToken?: string }) => {
-    const findUser = await prisma.user.findUnique({
-        where: {
-            email: payload.email
-        }
-    })
-    if (!findUser) {
-        throw new ApiError(StatusCodes.NOT_FOUND, "User not found")
-    }
-    const comparePassword = await compare(payload.password, findUser.password)
-    if (!comparePassword) {
-        throw new ApiError(StatusCodes.NON_AUTHORITATIVE_INFORMATION, "Invalid password")
-    }
+const logInFromDB = async (payload: {
+  email: string;
+  password: string;
+  fcmToken?: string;
+}) => {
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email: payload.email.trim(),
+    },
+  });
+  if (!findUser) {
+    throw new ApiError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  const comparePassword = await compare(payload.password, findUser.password);
+  if (!comparePassword) {
+    throw new ApiError(
+      StatusCodes.NON_AUTHORITATIVE_INFORMATION,
+      "Invalid password"
+    );
+  }
 
-    if (findUser.status === "PENDING") {
-        OTPFn(findUser.email)
-        throw new ApiError(401, "Please check your email address to verify your account")
-    }
+  if (findUser.status === "PENDING") {
+    OTPFn(findUser.email);
+    throw new ApiError(
+      401,
+      "Please check your email address to verify your account"
+    );
+  }
 
-    if (payload.fcmToken) {
-        await prisma.user.update({
-            where: {
-                email: payload.email
-            },
-            data: {
-                fcmToken: payload.fcmToken
-            }
-        })
-    }
-    const userInfo = {
-        email: findUser.email,
-        name: findUser.name,
-        id: findUser.id,
-        image : findUser.image,
-        role: findUser.role,
-        status: findUser.status,
-        fcmToken: findUser.fcmToken,
-    }
-    const token = jwtHelpers.generateToken(userInfo, { expiresIn: "24 hr" })
-    return { accessToken: token, ...userInfo }
-}
-
-const verifyOtp = async (payload: { email: string; otp: number }) => {
-
-    const { message } = await OTPVerify(payload)
-
-    if (message) {
-        const updateUserInfo = await prisma.user.update({
-            where: {
-                email: payload.email
-            },
-            data: {
-                status: "ACTIVE",
-                isVerified: true
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                role: true,
-                status: true,
-                isVerified: true,
-                createdAt: true,
-                updatedAt: true
-            }
-        })
-        return updateUserInfo
-    }
-
+  if (payload.fcmToken) {
+    await prisma.user.update({
+      where: {
+        email: payload.email,
+      },
+      data: {
+        fcmToken: payload.fcmToken,
+      },
+    });
+  }
+  const userInfo = {
+    email: findUser.email,
+    name: findUser.name,
+    id: findUser.id,
+    image: findUser.image,
+    role: findUser.role,
+    status: findUser.status,
+    fcmToken: findUser.fcmToken,
+  };
+  const token = jwtHelpers.generateToken(userInfo, { expiresIn: "24 hr" });
+  return { accessToken: token, ...userInfo };
 };
 
+const verifyOtp = async (payload: { email: string; otp: number }) => {
+  const { message } = await OTPVerify(payload);
 
-const verifyRestOtp = async(payload: {token: string; otp: number }) => {
-    const {accessToken} = await OTPVerify(payload)
-    return accessToken
-}
+  if (message) {
+    const updateUserInfo = await prisma.user.update({
+      where: {
+        email: payload.email,
+      },
+      data: {
+        status: "ACTIVE",
+        isVerified: true,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        status: true,
+        isVerified: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return updateUserInfo;
+  }
+};
 
 const forgetPassword = async (payload: { email: string }) => {
-    const findUser = await prisma.user.findUnique({
-        where: {
-            email: payload.email
-        }
-    })
-    if (!findUser) {
-        throw new Error("User not found")
-    }
-    const token = jwtHelpers.generateToken({ email: findUser.email, id: findUser?.id, role: findUser?.role }, { expiresIn: "1hr" }) as Secret
-    OTPFn(findUser.email)
-    return { accessToken: token }
-}
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
+    },
+  });
+  if (!findUser) {
+    throw new Error("User not found");
+  }
+  const token = jwtHelpers.generateToken(
+    { email: findUser.email, id: findUser?.id, role: findUser?.role },
+    { expiresIn: "1hr" }
+  ) as Secret;
+  OTPFn(findUser.email);
+  return { accessToken: token };
+};
 
 const resendOtp = async (payload: { email: string }) => {
-    const findUser = await prisma.user.findUnique({
-        where: {
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
+    },
+  });
+  if (!findUser) {
+    throw new ApiError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  OTPFn(findUser.email);
+};
 
-            email: payload.email
-        }
-    })
-    if (!findUser) {
-        throw new ApiError(StatusCodes.NOT_FOUND, "User not found")
+const socialLogin = async (payload: {
+  email: string;
+  name: string;
+  role: Role;
+  image?: string;
+}) => {
+  const userData = await prisma.user.findUnique({
+    where: {
+      email: payload.email.trim(),
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      image: true,
+      role: true,
+      customerId: true,
+      status: true,
+      connectAccountId: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  if (userData) {
+    const accessToken = jwtHelpers.generateToken(
+      { id: userData.id, email: userData.email, role: userData.role },
+      { expiresIn: "24 hr" }
+    );
+    return {
+      id: userData.id,
+      name: userData.name,
+      email: userData.email,
+      role: userData.role,
+      customerId: userData.customerId,
+      image: userData?.image,
+      status: userData.status,
+      accessToken: accessToken,
+    };
+  } else {
+    const result = await prisma.user.create({
+      data: {
+        ...payload,
+        password: "",
+        status: "ACTIVE",
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        image: true,
+        role: true,
+        customerId: true,
+        status: true,
+        connectAccountId: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    const stripeCustomer = await stripe.customers.create({
+      email: payload.email.trim(),
+      name: payload.name || undefined,
+    });
+
+    if (!stripeCustomer.id) {
+      throw new ApiError(
+        StatusCodes.EXPECTATION_FAILED,
+        "Failed to create a Stripe customer"
+      );
     }
-    OTPFn(findUser.email)
-}
 
+    await prisma.user.update({
+      where: { id: result.id },
+      data: { customerId: stripeCustomer.id },
+    });
 
-export const authService = { logInFromDB, forgetPassword, verifyOtp, resendOtp, verifyRestOtp }
+    const accessToken = jwtHelpers.generateToken(
+      { id: result.id, email: result.email, role: result.role },
+      { expiresIn: "24 hr" }
+    );
+    return {
+      id: result.id,
+      name: result.name,
+      email: result.email,
+      role: result.role,
+      customerId: result.customerId,
+      image: result?.image,
+      status: result.status,
+      accessToken: accessToken,
+    };
+  }
+};
 
+export const authService = {
+  logInFromDB,
+  forgetPassword,
+  verifyOtp,
+  resendOtp,
+};
