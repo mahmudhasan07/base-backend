81f89954f93461b6384df4c066454d4d9ee741b6
Mahmud Hasan Siddique <mahmudhasan.hb@gmail.com>
2025-05-12 13:54:26 +0600

    Delete dist directory

--- dist/config/secret.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwt_secret = exports.prot = void 0;
-require("dotenv/config");
-exports.prot = process.env.PORT;
-exports.jwt_secret = process.env.TOKEN_SECRET;
