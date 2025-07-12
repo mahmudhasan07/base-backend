3e5646e540abe4156bf0812f3ec1d4a5d9a12be7
unknown <mahmudhasan.hb@gmail.com>
2025-07-12 18:43:32 +0600

    working on payment system

--- src/app.ts
+++ src/app.ts
@@ -1,72 +1,73 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import NodeCache from 'node-cache';
-import router from './app/route/route';
-import GlobalErrorHandler from './app/middleware/globalErrorHandler';
-import { MongoClient } from "mongodb"
-import { StatusCodes } from 'http-status-codes';
-import { PrismaConnection } from './app/DB/PrismaConnection';
-import path from 'path';
-import fs from 'fs';
-import morgan from 'morgan';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import NodeCache from "node-cache";
+import router from "./app/route/route";
+import GlobalErrorHandler from "./app/middleware/globalErrorHandler";
+import { MongoClient } from "mongodb";
+import { StatusCodes } from "http-status-codes";
+import { PrismaConnection } from "./app/DB/PrismaConnection";
+import path from "path";
+import fs from "fs";
+import morgan from "morgan";
+import { title } from "process";
 
-export const myCache = new NodeCache({ stdTTL: 300 })
+export const myCache = new NodeCache({ stdTTL: 300 });
 const app = express();
 
 export const corsOptions = {
-    origin: [
-        "http://localhost:3000",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+  origin: ["http://localhost:3000", "https://dashboard.mrphrenfix.com"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
-app.use(morgan('dev')); // 'dev' is a predefined format
+app.use(morgan("dev")); // 'dev' is a predefined format
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    res.send('Welcome to development world');
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    title: `${process.env.CLIENT_NAME} API`,
+    message: "Hello World!",
+  });
 });
 
 const uploadPath = path.join(__dirname, "..", "uploads");
 
 // Ensure uploads folder exists
 if (!fs.existsSync(uploadPath)) {
-    fs.mkdirSync(uploadPath, { recursive: true });
-    console.log("Uploads folder created successfully!");
+  fs.mkdirSync(uploadPath, { recursive: true });
+  console.log("Uploads folder created successfully!");
 }
 
 app.use("/uploads", express.static(uploadPath));
 
-
 const connectDB = async () => {
-    try {
-        const conn = await new MongoClient(process.env.DATABASE_URL as string).connect();
-        console.log(`MongoDB Connected Successfully`);
-    } catch (error: any) {
-        console.error(`Error: ${error?.message}`);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    const conn = await new MongoClient(
+      process.env.DATABASE_URL as string
+    ).connect();
+    console.log(`MongoDB Connected Successfully`);
+  } catch (error: any) {
+    console.error(`Error: ${error?.message}`);
+    process.exit(1); // Exit process with failure
+  }
 };
 
-connectDB()
+connectDB();
 
-app.use("/api/v1", router)
-app.use(GlobalErrorHandler)
-
+app.use("/api/v1", router);
+app.use(GlobalErrorHandler);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(StatusCodes.NOT_FOUND).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!",
-        },
-    });
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
 });
 
-
-
-export default app
+export default app;
